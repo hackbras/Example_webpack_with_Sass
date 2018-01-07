4192340f3110204d3501c8ba5e121000036c45a4
vm-w7x64ult-PC\vm-w7x64ult <hackbras@outlook.com>
2018-01-07 01:05:41 -0300

    Learning werpack. Changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,36 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
+// module.exports = {
+//     entry: './src/index.js',
+//     output: {
+//         path: __dirname + '/dist',
+//         filename: 'index.bundle.js'
+//     },
+//     plugins: [new HtmlWebpackPlugin()]
+// }
+
 module.exports = {
     entry: './src/index.js',
     output: {
-        //path: './dist',
-        filename: 'dist/index.bundle.js'
+        path: __dirname + '/dist',
+        filename: 'index.bundle.js'
     },
-    module: {
-        rules: [
-            { test: /\.scss/, use: ['style-loader', 'css-loader', 'sass-loader'] }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin(),
+    // module: {
+    //     rules: [{
+    //             test: /\.sass/,
+    //             use: ['style-loader', 'css-loader', 'sass-loader']
+    //         },
+    //         {
+    //             test: /\.pug/,
+    //             use: ExtractTextPlugin.extract({
+    //                 fallbackloader: 'html-loader',
+    //                 loader: ['pug-html-loader', 'pug-loader'],
+    //                 publicPath: '/dist'
+    //             })
+    //         }
+    //     ]
+    // },
+    plugins: [new HtmlWebpackPlugin()]
 
-        new ExtractTextPlugin({
-            filename: 'style.css',
-            disable: false,
-            allChunks: true
-        })
-    ]
 }
